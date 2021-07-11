4246cbe618bc4fa2610f4b2390115c2054310fcf
iamaditia <iamaditia@users.noreply.github.com>
2021-07-11 02:23:30 +0000

    jekyll build from Action f0beaf5bc1e1f3953acd16985b8ac7750c5cfd92

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "My Startup Goal for 2021",
+        "excerpt":"Every indie makers out there like to post their goals in public. Partly for accountability, partly for self-motivation. Fine, I’ll post mine here. My startup goal for 2021 is $5 ARR. Yes, you read that right–that’s not a typo. Five bucks a year. That’s my goal. In the age of...","categories": ["Build in Public"],
+        "tags": ["goal","happiness"],
+        "url": "/build%20in%20public/my-startup-goal-for-2021/",
+        "teaser": null
+      }]
